eeab0cdb9724350d21c5bd5964acdf9ccb273079
Sebastian-Henao <sebastian.henao03@uceva.edu.co>
2026-09-25 11:28:42 -0500

    feat(Cliente: personajes): Se implementa servicio para personajes

--- Client-Angular/src/app/services/personajes/personajes.service.ts
+++ Client-Angular/src/app/services/personajes/personajes.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Personaje } from '../../interfaces/personajes.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PersonajesService {
-  
+  private httpClient = inject(HttpClient);
+  getAllPersonajes(countPersonajes: number): Observable<Personaje[]> {
+    return this.httpClient.get<Personaje[]>(`api/personajes/${countPersonajes}`);
+  }
 }
