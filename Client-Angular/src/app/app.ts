5c0a8ac4ef583b17328ec49af22f7153107c3c33
Sebastian-Henao <sebastian.henao03@uceva.edu.co>
2026-09-25 11:41:45 -0500

    feat(Cliente: personajes): Se implementa boton de personajes en la navbar y la navegacion hacia la pagina

--- Client-Angular/src/app/app.ts
+++ Client-Angular/src/app/app.ts
@@ -58,6 +58,7 @@ export class App {
     navLinks: [
       { text: 'Usuarios', url: '/users' },
       { text: 'Productos', url: '/products' },
+      { text: 'Personajes', url: '/personajes' },
     ]
   };
 }
