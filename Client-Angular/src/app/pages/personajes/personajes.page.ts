6f6202da4206177399027c498ce32fbd2870b245
Sebastian-Henao <sebastian.henao03@uceva.edu.co>
2026-09-25 11:37:41 -0500

    feat(Cliente: personajes): Se implementa pagina para mostrar la tabla de personajes

--- Client-Angular/src/app/pages/personajes/personajes.page.ts
+++ Client-Angular/src/app/pages/personajes/personajes.page.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductsTableComponent } from '../../components/products-table/products-table.component';
+import { AlertComponent } from '../../components/alert/alert.component';
+import { Personaje } from '../../interfaces/personajes.interface';
+import { PersonajesService } from '../../services/personajes/personajes.service';
+import { State } from '../../interfaces/state.interface'
 
 @Component({
-  selector: 'app-personajes.page',
-  imports: [],
+  selector: 'app-personajes',
+  imports: [ProductsTableComponent, AlertComponent],
   templateUrl: './personajes.page.html',
-  styleUrl: './personajes.page.scss',
 })
 export class PersonajesPage {
-
+  personajes: Personaje[] = [];
+    state: State = 'init';
+  private personajesService = inject(PersonajesService);
+  ngOnInit(): void {
+    this.state = 'loading';
+    this.personajesService.getAllPersonajes(10).subscribe({
+      next: (personajes) => {
+        this.personajes = personajes;
+        this.state = 'success';
+      },
+      error: (error) => {
+        console.error(error)
+        this.state = 'error';
+      },
+    })
+  }
 }
