a916f7d3a7cdc784995b1fdffecc34085e952cf6
Sebastian-Henao <sebastian.henao03@uceva.edu.co>
2026-09-25 11:10:56 -0500

    feat(Cliente: personajes): Se implementa el componente de la tabla de personajes

--- Client-Angular/src/app/components/personajes-table/personajes-table.component.ts
+++ Client-Angular/src/app/components/personajes-table/personajes-table.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { BadgeAtom, BadgeType } from '@brejcha13320/design-system-bootstrap';
+import { Personaje, RolCategoria } from '../../interfaces/personajes.interface';
 
 @Component({
-  selector: 'app-personajes-table.component',
-  imports: [],
+  selector: 'app-personajes-table',
+  imports: [CommonModule, BadgeAtom],
   templateUrl: './personajes-table.component.html',
-  styleUrl: './personajes-table.component.scss',
 })
 export class PersonajesTableComponent {
-
+  @Input() personajes: Personaje[] = [];
+  categoryMap: Record<RolCategoria, BadgeType> = {
+    'Vanguardia': 'primary',
+    'Ladron': 'dark',
+    'Jefe': 'danger',
+    'Ilusionista': 'info',
+    'Explorador': 'success',
+  }
 }
