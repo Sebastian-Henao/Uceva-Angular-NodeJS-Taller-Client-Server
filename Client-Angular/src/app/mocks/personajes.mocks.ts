60bc80638ac17dedc78bbd96df3a0805483f4df2
Sebastian-Henao <sebastian.henao03@uceva.edu.co>
2026-09-25 11:23:02 -0500

    feat(Cliente: personajes): Se implementa mocks de personaje

---
+++ Client-Angular/src/app/mocks/personajes.mocks.ts
@@ -0,0 +1,20 @@
+import { Personaje } from "../interfaces/personajes.interface";
+
+export const PERSONAJES_MOCK: Personaje[] = [
+    {
+        id: 1,
+        nombre: 'Loki',
+        alias: 'Dios de las mentiras',
+        juego: 'Marvel Rivals',
+        habilidad: 'Hechicero',
+        rol: 'Ilusionista'
+    },
+    {
+        id: 2,
+        nombre: 'Nathan Drake',
+        alias: 'Nate',
+        juego: 'Uncharted',
+        habilidad: 'Experto en armas y supervivencia',
+        rol: 'Ladron'
+    }
+];
