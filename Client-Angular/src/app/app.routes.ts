b9e528f7cbd2a55c0f852153ee67c72d9f6084ef
Kemelo996 <kevin.montes03@uceva.edu.co>
2026-09-26 11:58:00 -0500

    feat(Cliente: personajes-midlane): Se implementa boton en la navbar y la navegacion hacia la pagina

--- Client-Angular/src/app/app.routes.ts
+++ Client-Angular/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { UsersPage } from './pages/users/users.page';
 import { ProductsPage } from './pages/products/products.page';
 import { PersonajesPage } from './pages/personajes/personajes.page';
+import { MidlanersPage } from './pages/midlaners/midlaners.page';
 
 /**
  * Definición de las rutas principales de la aplicación.
@@ -39,6 +40,7 @@ export const routes: Routes = [
   { path: 'products', component: ProductsPage },
 
   {path: 'personajes', component: PersonajesPage},
+  { path: 'midlaners', component: MidlanersPage },
 
   /**
    * Ruta comodín.
--- Client-Angular/src/app/app.spec.ts
+++ Client-Angular/src/app/app.spec.ts
@@ -35,7 +35,8 @@ describe('App', () => {
       navLinks: [
         { text: 'Usuarios', url: '/users' },
         { text: 'Productos', url: '/products' },
-        { text: 'Personajes', url: '/personajes' }
+        { text: 'Personajes', url: '/personajes' },
+        { text: 'LoL Midlane', url: '/midlaners' }
       ]
     });
   });
