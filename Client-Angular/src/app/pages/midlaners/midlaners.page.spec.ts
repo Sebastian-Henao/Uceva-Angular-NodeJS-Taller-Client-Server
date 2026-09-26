afbc7fc02ba5f3af0fd470133e02022245847a18
Kemelo996 <kevin.montes03@uceva.edu.co>
2026-09-26 11:55:47 -0500

    feat(Cliente: personajes-midlane): Se implementa pagina para mostrar la tabla

--- Client-Angular/src/app/pages/midlaners/midlaners.page.spec.ts
+++ Client-Angular/src/app/pages/midlaners/midlaners.page.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { MidlanersPage } from './midlaners.page';
+import { MidlanersService } from '../../services/midlaners/midlaners.service';
 
 describe('MidlanersPage', () => {
   let component: MidlanersPage;
@@ -8,6 +10,12 @@ describe('MidlanersPage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MidlanersPage],
+      providers: [
+        {
+          provide: MidlanersService,
+          useValue: { getAllMidlaners: () => of([]) },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MidlanersPage);
--- Client-Angular/src/app/pages/midlaners/midlaners.page.ts
+++ Client-Angular/src/app/pages/midlaners/midlaners.page.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AlertComponent } from '../../components/alert/alert.component';
+import { MidlanersTableComponent } from '../../components/midlaners-table/midlaners-table.component';
+import { State } from '../../interfaces/state.interface';
+import { Midlaner } from '../../interfaces/midlaner.interface';
+import { MidlanersService } from '../../services/midlaners/midlaners.service';
 
 @Component({
   selector: 'app-midlaners-page',
-  imports: [],
+  imports: [AlertComponent, MidlanersTableComponent],
   templateUrl: './midlaners.page.html',
 })
-export class MidlanersPage {}
+export class MidlanersPage {
+  midlaners: Midlaner[] = [];
+  state: State = 'init';
+
+  private midlanersService = inject(MidlanersService);
+
+  ngOnInit(): void {
+    this.state = 'loading';
+    this.midlanersService.getAllMidlaners().subscribe({
+      next: (midlaners) => {
+        this.midlaners = midlaners;
+        this.state = 'success';
+      },
+      error: (error) => {
+        console.error(error);
+        this.state = 'error';
+      },
+    });
+  }
+}
