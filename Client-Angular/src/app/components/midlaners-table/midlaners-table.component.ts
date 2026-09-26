c223f2b8792ed370b85f463ce241a61443710587
Kemelo996 <kevin.montes03@uceva.edu.co>
2026-09-26 11:50:05 -0500

    feat(Cliente: personajes-midlane): Se implementa el componente de la tabla

--- Client-Angular/src/app/components/midlaners-table/midlaners-table.component.ts
+++ Client-Angular/src/app/components/midlaners-table/midlaners-table.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Midlaner } from '../../interfaces/midlaner.interface';
 
 @Component({
   selector: 'app-midlaners-table',
   imports: [],
   templateUrl: './midlaners-table.component.html',
 })
-export class MidlanersTableComponent {}
+export class MidlanersTableComponent {
+  /** Campeones que se presentarán en la tabla. */
+  @Input() midlaners: Midlaner[] = [];
+}
