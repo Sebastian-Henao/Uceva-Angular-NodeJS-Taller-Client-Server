279a30a6f7464ac2137ddf371edba0e3bd09d323
Kemelo996 <kevin.montes03@uceva.edu.co>
2026-09-26 11:10:48 -0500

    feat(LoL-midlane): Se implementa rutas para personajes

--- Server-NodeJS/src/presentation/modules/midlaners/midlaners.routes.ts
+++ Server-NodeJS/src/presentation/modules/midlaners/midlaners.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import { MidlanersController } from "./midlaners.controller";
 
 export class MidlanersRoutes {
     static get routes(): Router {
         const router = Router();
+        const controller = new MidlanersController();
+
+        router.get("/", controller.getAllMidlaners);
+
         return router;
     }
 }
